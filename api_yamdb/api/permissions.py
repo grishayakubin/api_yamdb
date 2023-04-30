2372c3a11ba52b4c2afe0181453ee55972a8dcb3
Grisha Yakubin <g.yakubin03@gmail.com>
2023-05-01 00:53:35 +0600

    full project

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import SAFE_METHODS, IsAdminUser
+from rest_framework import permissions
 
 
 class IsAdminUserOrReadOnly(IsAdminUser):
@@ -16,3 +17,15 @@ class IsAdminUserOrReadOnly(IsAdminUser):
             IsAdminUserOrReadOnly,
             self).has_permission(request, view)
         return request.method in SAFE_METHODS or is_admin
+
+
+class IsAdmin(permissions.BasePermission):
+    """IsAdmin permission.
+    Разрешает доступ к ресурсу, если пользователь аутентифицирован и является
+    администратором или суперпользователем.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (
+            request.user.is_admin or request.user.is_superuser
+        )
